aee20a18a1bf1768ee5c817767572b7c0b0a5a9a
ushan <ushanchamodbandara@gmail.com>
2025-04-15 13:47:19 +0530

    l

--- src/components/ui/BasicSelect.tsx
+++ src/components/ui/BasicSelect.tsx
@@ -1,4 +1,3 @@
-import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
